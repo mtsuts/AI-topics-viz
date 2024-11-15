e2c34cafd1fb9cf6fea242d02a43504774339fb6
mtsuts <mariamtsuts@gmail.com>
2024-11-15 14:30:04 -0800

    init project with new data and design

---
+++ chart/legend.js
@@ -0,0 +1,11 @@
+const legendContainer = d3.select('#legend_container')
+
+labelsData.map((label, index) => {
+  legendContainer
+    .append('button')
+    .attr('class', 'legend-button')
+    .attr('id', label.data_type)
+    .attr('data-type', label.data_type)
+    .style('background-color', label.background_color)
+    .text(label.topic)
+})
