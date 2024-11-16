c7648288d346cf0779ace8d4bdc53090afdb6d85
mtsuts <mariamtsuts@gmail.com>
2024-11-15 16:42:35 -0800

    added quotes section

--- chart/chart.js
+++ chart/chart.js
@@ -1,8 +1,5 @@
 function forceGraph(nodes, params) {
-  const container = d3.select(params.container);
-  const formatTime = d3.timeFormat("%d %B, %Y")
-  let simulation;
-  let isClicked = false;
+  const container = d3.select(params.container)
   // define metrics
 
   const bounding = container.node().getBoundingClientRect()
@@ -12,7 +9,7 @@ function forceGraph(nodes, params) {
     height: bounding.height,
     chartWidth: bounding.width,
     chartHeight: bounding.height
-  };
+  }
 
   // create svg and append to container
   const svg = d3
@@ -26,21 +23,23 @@ function forceGraph(nodes, params) {
     .attr('width', metrics.chartWidth)
     .attr('height', metrics.chartHeight)
 
-
+  // radius scale
   const rScale = d3
     .scaleLinear()
     .domain([
       d3.min(nodes.map((d) => d.amount)),
       d3.max(nodes.map((d) => d.amount)),
     ])
-    .range([40, 100]);
+    .range([40, 100])
 
+  // font size scale
   const fontSizeScale = d3.scaleLinear().domain([
     d3.min(nodes.map((d => d.amount))),
     d3.max(nodes.map((d) => d.amount))
   ]).range([10, 12])
 
 
+  // create node g element
   const node = g
     .selectAll("g.node")
     .data(nodes)
@@ -50,24 +49,16 @@ function forceGraph(nodes, params) {
     .attr('cursor', 'pointer')
     .attr('id', (d) => d.id)
 
-  let r;
 
+  // draw node circle
   node
     .append("circle")
     .attr('class', (d) => `circle-node`)
     .attr("r", (d) => rScale(d.amount))
     .attr('fill', (d) => d.color)
     .attr('opacity', 0.3)
-    .on('mouseover', function () {
-      d3.select(this)
-      .transition().duration("100")
-      .attr('opacity', 1)
-      .style('filter', 'brightness(1.3')
-    })
-    .on('mouseout', function (d) {
-      d3.select(this).transition().duration("100").attr("r", (x) => rScale(x.amount)).attr('opacity', 0.5)
-    })
 
+  // append text to node circle
   node
     .append("text")
     .attr("font-size", (d) => fontSizeScale(d.amount))
@@ -85,9 +76,9 @@ function forceGraph(nodes, params) {
 
   node.on("click", function (e, d) {
     onNodeClick(e, d)
-    // quotesOpened(d)
   })
 
+  // force graph simulation
   function forceSimul() {
     simulation = d3
       .forceSimulation(nodes)
@@ -96,22 +87,22 @@ function forceGraph(nodes, params) {
         "collision",
         d3
           .forceCollide()
-          .radius((d) => rScale(d.amount) + 10)
+          .radius((d) => rScale(d.amount) + 4)
           .iterations(10)
       )
       .force('charge', d3.forceManyBody().strength(40))
-      .force("x", d3.forceX().strength(0.00008))
+      .force("x", d3.forceX().strength(0.0008))
       .force("y", d3.forceY().strength(0.09))
       .on("tick", function ticked() {
-        node.attr("transform", (d) => `translate(${d.x}, ${d.y})`);
-      });
-
+        node.attr("transform", (d) => `translate(${d.x}, ${d.y})`)
+      })
   }
+
   forceSimul()
 
+  // handle circle node click event
   function onNodeClick(e, d) {
-    // simulation.alpha(0.2).restart()
-    isClicked = true
+    // forceSimul()
     d3.selectAll('.circle-node').style('filter', `drop-shadow(0 0 1.2rem transparent)`).classed('click', false)
     quotesOpened(d)
 
@@ -128,78 +119,53 @@ function forceGraph(nodes, params) {
       .duration(1000)
       .style('opacity', 1)
   }
+
+
+  function quotesOpened(topic) {
+    // quotes container
+    const container = d3.select('#quotes')
+    // quotes data
+    const quotes = topic.data
+
+    // apply styles to chosen node
+    d3.select(`#${topic.id}`)
+      .select('.circle-node')
+      .style('opacity', 1)
+      .classed('click', true)
+      .attr('r', (x) => rScale(x.amount))
+      .style('filter', `drop-shadow(0 0 0.90rem ${topic.color})`)
+
+    // clean container before append new topic quotes
+    container.html('')
+
+    // append quotes data to container
+    quotes.map((quote) => {
+
+
+      // append quote container
+      const quoteContainer = container
+        .append("div")
+        .attr('class', 'quote-container')
+
+      quoteContainer.append('div')
+        .html(`<div class='quote-container-text'> 
+        <div class='quote-author'>  ${quote.person} <span> (${quote.role})</span>  </div>
+        <div class='quote-text'> "${quote.quote}"</div> 
+        <div class='quote-source'> 
+        
+        <a href=${quote.source} target='_blank'> 
+        Read more 
+<svg width="16px" height="16px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Interface / External_Link"> <path id="Vector" d="M10.0002 5H8.2002C7.08009 5 6.51962 5 6.0918 5.21799C5.71547 5.40973 5.40973 5.71547 5.21799 6.0918C5 6.51962 5 7.08009 5 8.2002V15.8002C5 16.9203 5 17.4801 5.21799 17.9079C5.40973 18.2842 5.71547 18.5905 6.0918 18.7822C6.5192 19 7.07899 19 8.19691 19H15.8031C16.921 19 17.48 19 17.9074 18.7822C18.2837 18.5905 18.5905 18.2839 18.7822 17.9076C19 17.4802 19 16.921 19 15.8031V14M20 9V4M20 4H15M20 4L13 11" stroke="#fffafa" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>
+        </a>
+        
+        </div>
+        </div>`)
+
+    })
+  }
+
+  quotesOpened(nodes.find(d => d.terminology === "Creativity"))
 }
 
 
-function quotesOpened(d){
-  console.log(d)
-}
 
-
-    // function sectionQuotes(section, side, sectionNumber) {
-    //   section
-    //     .selectAll("div.quote")
-    //     .data(d.quotes.filter(d => d.side === side))
-    //     .join("div")
-    //     .attr("class", "quote")
-    //     .html((x) =>
-    //       `
-    // <div class="row align-items-start ${sectionNumber === 2 ? 'flex-row-reverse' : ''}"> 
-    
-    // <div class="col-3 message-author" data-tippy-content="${x.regalia}"> <img class="author-image" src = "${x.photo}"> </img> 
-    // <div class='author-name'> ${x.author}</div> 
-    // </div>
-    // <div class="col-9 message-quote"> <div class='quote-date'>${formatTime(x.date)}</div> <div>${colorWords(x.quote, d)}</div> </div>
-    // </div>
-    // <div class="d-flex quote-underline-tv align-items-start ${sectionNumber === 2 ? 'flex-row-reverse' : ''}"> 
-    // <div class='quote-underline'> </div>
-    // <div class='tv ${sectionNumber === 2 ? 'pr-1' : 'pl-1'}'> <a class="tv-link" href="${x.link}" target="_blank" > ${x.tv} </a> </div> 
-    // </div>
-    // `)
-    // }
-
-
-        // sectionQuotes(quotes_first, sideOne, sectionFirst)
-    // sectionQuotes(quotes_second, sideTwo, sectionSecond)
-
-
-      // function quotesOpened(d) {
-  //   const sideOne = 'ხელისუფლების წარმომადგენლები'
-  //   const sideTwo = 'სხვები'
-  //   const quotes_first = d3.select('#quotes_first')
-  //   const quotes_second = d3.select('#quotes_second')
-  //   const sectionFirst = 1
-  //   const sectionSecond = 2
-
-  //   d3.select(`#${d.id}`)
-  //     .select('.circle-node')
-  //     .classed('click', true)
-  //     .attr('r', (x) => rScale(x.amount))
-  //     .style('filter', `drop-shadow(0 0 0.90rem ${d.color})`)
-
-
-
-  //   function colorWords(quote, d) {
-  //     return quote.replaceAll(`${d.terminology}`, `<span style="background-color: ${d.color}; opacity: 0.6"> ${d.terminology} </span>`)
-  //   }
-  //   d3.selectAll('.circle-node').attr('stroke', 'none').attr('opacity', 0.5)
-
-  //   d3.select('.quotes-section').transition().duration(1000).style('opacity', 1)
-
-  //   d3.select("#terminologies").style('border-bottom-color', d.quotes[0].color)
-  //     .html(`<div> ${d.terminology}    
-  //     </div>`)
-
-  //   d3.select('#message_line')
-  //     .html(`<div> ${d.message} </div>`)
-
-
-  //   d3.selectAll(`.message-author`).each(function () {
-  //     tippy('[data-tippy-content]', {
-  //       arrow: false,
-  //       theme: 'custom'
-  //     })
-  //   })
-  // }
-
-  // quotesOpened(nodes.find(d => d.terminology === "ევროპარლამენტი"),)
